baeda14b2a4b527a8eb140f00b32aa180186a560
dimitriinc <dimitriinc@proton.me>
2025-11-04 21:59:03 -0500

    playing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   def home
     @message = "Hello from Rails!"
     @version = ActiveRecord::Base.connection.execute("SELECT version()").first["version"]
+    @id = params[:id]
   end
 end
